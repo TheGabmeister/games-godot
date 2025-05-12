cccdfb1b47d822cb05bfe412ba7dc5eafeb80247
TheGabmeister <thegabmeister@protonmail.com>
2025-05-12 18:58:46 +0800

    Update player movement

--- space_invaders/scripts/player_movement.gd
+++ space_invaders/scripts/player_movement.gd
@@ -2,18 +2,19 @@ extends Node2D
 
 # Speed of the player in pixels per second
 var speed := 200
+var boundary := 350
 
 func _process(delta):
 	var direction := Vector2.ZERO
-
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
+
+	if position.x > boundary:
+		position.x = boundary
+	if position.x < -boundary:
+		position.x = -boundary
 
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
