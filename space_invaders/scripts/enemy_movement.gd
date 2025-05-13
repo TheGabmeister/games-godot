05ec85a1c9bf1382cfc1cdbfeed6ab577c894494
TheGabmeister <thegabmeister@protonmail.com>
2025-05-13 12:31:54 +0800

    Add game manager

--- space_invaders/scripts/enemy_movement.gd
+++ space_invaders/scripts/enemy_movement.gd
@@ -13,19 +13,21 @@ func _process(delta: float) -> void:
 	if (position.x > BOUNDARY):
 		_move_vertical()
 		direction = -1
-	if (position.x < -BOUNDARY):
+	if position.x < -BOUNDARY:
 		_move_vertical()
 		direction = 1
 	position.x += movement_speed * direction * delta
 
 # when moving down, prevent other instances from triggering the group movement
 func _move_vertical():
-	if(!has_moved_vertical):
-		has_moved_vertical = true
-		for enemy in get_parent().get_children():
-			if enemy.is_in_group("enemies"):
-				enemy.position.y += VERTICAL_MOVEMENT
-		call_deferred("_reset_has_moved_vertical")
+	if has_moved_vertical:
+		return
+	
+	has_moved_vertical = true
+	for enemy in get_parent().get_children():
+		if enemy.is_in_group("enemies"):
+			enemy.position.y += VERTICAL_MOVEMENT
+	call_deferred("_reset_has_moved_vertical")
 
 func _reset_has_moved_vertical():
 	has_moved_vertical = false
