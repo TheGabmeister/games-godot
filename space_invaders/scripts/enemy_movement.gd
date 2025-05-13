cd8dd8bda57fce638d109c7e22277b1594ce26da
TheGabmeister <thegabmeister@protonmail.com>
2025-05-13 08:56:39 +0800

    Add vertical movement

--- space_invaders/scripts/enemy_movement.gd
+++ space_invaders/scripts/enemy_movement.gd
@@ -1,15 +1,31 @@
 extends Node2D
 
 var movement_speed := 300.0
+const VERTICAL_MOVEMENT := 30
 const BOUNDARY := 350
 static var direction := 1
+static var has_moved_vertical := false
 
 func _ready() -> void:
 	pass 
 
 func _process(delta: float) -> void:
 	if (position.x > BOUNDARY):
+		_move_vertical()
 		direction = -1
 	if (position.x < -BOUNDARY):
+		_move_vertical()
 		direction = 1
 	position.x += movement_speed * direction * delta
+
+# when moving down, prevent other instances from triggering the group movement
+func _move_vertical():
+	if(!has_moved_vertical):
+		has_moved_vertical = true
+		for enemy in get_parent().get_children():
+			if enemy.is_in_group("enemies"):
+				enemy.position.y += VERTICAL_MOVEMENT
+		call_deferred("_reset_has_moved_vertical")
+
+func _reset_has_moved_vertical():
+	has_moved_vertical = false
