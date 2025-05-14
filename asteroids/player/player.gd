1ab8dc8589606d0bd8f600911e39156046f118fe
TheGabmeister <thegabmeister@protonmail.com>
2025-05-15 00:03:41 +0800

    Add fire cooldown

--- asteroids/player/player.gd
+++ asteroids/player/player.gd
@@ -3,10 +3,12 @@ class_name Player
 
 const SPEED := 300.0
 const ROTATION_SPEED := 5.0
-const COOLDOWN := 1
+const COOLDOWN := 1.0
+var _cooldown_remaining := 1.0
 #var bullet: PackedScene = preload("res://player/player_laser.tscn")
 
 func _process(delta):
+
 	if Input.is_action_pressed("move_right"):
 		rotation += ROTATION_SPEED * delta
 	if Input.is_action_pressed("move_left"):
@@ -15,6 +17,9 @@ func _process(delta):
 		position += Vector2.UP.rotated(rotation) * SPEED * delta
 	if Input.is_action_pressed("move_down"):
 		position -= Vector2.UP.rotated(rotation) * SPEED * delta
-	
-	if Input.is_action_pressed("fire"):
-		pass
+
+	if _cooldown_remaining > 0:
+		_cooldown_remaining -= delta
+	if Input.is_action_pressed("fire") && _cooldown_remaining < 0:
+		print("Fired")
+		_cooldown_remaining = COOLDOWN
