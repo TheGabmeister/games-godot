bb79a0b75af060de1618ce556241a84bfbc548cb
TheGabmeister <thegabmeister@protonmail.com>
2025-05-14 14:16:00 +0800

    trigger event when enemy killed

--- space_invaders/scripts/enemy_movement.gd
+++ space_invaders/scripts/enemy_movement.gd
@@ -5,8 +5,8 @@ const SCORE: int = 100
 var movement_speed: float = 300.0
 const VERTICAL_MOVEMENT: float = 30.0
 const BOUNDARY: float = 700.0
-static var direction: int = 1
-static var has_moved_vertical: bool = false
+static var _direction: int = 1
+static var _has_moved_vertical: bool = false
 
 func _ready() -> void:
 	GameEvents.enemy_killed.emit(SCORE)
@@ -15,18 +15,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if position.x > BOUNDARY:
 		_move_vertical()
-		direction = -1
+		_direction = -1
 	if position.x < 0:
 		_move_vertical()
-		direction = 1
-	position.x += movement_speed * direction * delta
+		_direction = 1
+	position.x += movement_speed * _direction * delta
 
 # when moving down, prevent other instances from triggering the group movement
 func _move_vertical() -> void:
-	if has_moved_vertical:
+	if _has_moved_vertical:
 		return
 	
-	has_moved_vertical = true
+	_has_moved_vertical = true
 
 	var enemies: Array = get_parent().get_children().filter(func(n: Node) -> bool: return n is Node2D)
 	for enemy: Node2D in enemies:
@@ -36,7 +36,8 @@ func _move_vertical() -> void:
 	call_deferred("_reset_has_moved_vertical")
 
 func _reset_has_moved_vertical() -> void:
-	has_moved_vertical = false
+	_has_moved_vertical = false
 
 func die() -> void:
-	pass
+	GameEvents.enemy_killed.emit(SCORE)
+	queue_free()
