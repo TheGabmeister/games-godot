a395cacc5e55d45612a31d27e1054c58567c99fa
TheGabmeister <thegabmeister@protonmail.com>
2025-05-15 09:39:27 +0800

    Add on_hit

--- asteroids/projectiles/projectile.gd
+++ asteroids/projectiles/projectile.gd
@@ -1,13 +1,6 @@
 extends Node2D
 
-@export var speed: float = 300.0
-@export var target_group: String = ""
-
-func _process(delta: float) -> void:
-	position.y += speed * delta
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-#	if area.get_parent().has_meta(IHittable.IHITTABLE):
-#		var i: IHittable = area.get_parent().get_meta(IHittable.IHITTABLE)
-#		i.on_hit(self)
+func _on_area_2d_area_entered(area: Area2D):
+	if area.has_method("on_hit"):
+		area.on_hit()
 	queue_free()
