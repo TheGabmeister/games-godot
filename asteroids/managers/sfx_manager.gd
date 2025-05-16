848fd15bb5e420b1d06abdb2174402ae0a994355
TheGabmeister <thegabmeister@protonmail.com>
2025-05-16 15:06:41 +0800

    Implement polyphony

--- asteroids/managers/sfx_manager.gd
+++ asteroids/managers/sfx_manager.gd
@@ -1,5 +1,8 @@
 extends AudioStreamPlayer
 
+func _ready():
+	stream = AudioStreamPolyphonic.new()
+	stream.polyphony = 10
 func _enter_tree():
 	Bus.sfx_play_sound.connect(_play_sound)
 
@@ -7,5 +10,9 @@ func _exit_tree():
 	Bus.sfx_play_sound.disconnect(_play_sound)
 	
 func _play_sound(sound: AudioStream):
-	stream = sound
+	
+	if !playing: self.play()
+	
+	var polyphonic_stream_playback := get_stream_playback()
+	polyphonic_stream_playback.play_stream(sound)
 	play()
