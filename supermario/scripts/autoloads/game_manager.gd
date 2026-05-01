994ecc2ecc4c46e264b1111c089c517ab2e36082
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-05-01 11:22:26 +0800

    Move back gameplay_manager to game_manager

--- supermario/scripts/autoloads/game_manager.gd
+++ supermario/scripts/autoloads/game_manager.gd
@@ -6,18 +6,39 @@ enum GameState { TITLE, PLAYING, PAUSED, GAME_OVER, LEVEL_COMPLETE, TRANSITIONIN
 
 # .gd autoloads can't use @export, so game-wide config lives in a .tres editable in the inspector.
 const _config := preload("res://resources/config/game_config.tres")
+const _player_scene := preload("res://scenes/player/player.tscn")
 
 var score: int = 0
 var coins: int = 0
 var lives: int = 3
+var time_remaining: float = 0.0
 var current_power_state: PowerState = PowerState.SMALL
 var game_state: GameState = GameState.TITLE
 
 var _level_index: int = 0
+var _last_time_tick: int = -1
+var _timer_active: bool = false
+var _player: CharacterBody2D
 
 
 func _ready() -> void:
+	EventBus.flagpole_reached.connect(_on_flagpole_reached)
 	EventBus.level_completed.connect(_on_level_completed)
+
+
+func _process(delta: float) -> void:
+	if _timer_active and game_state == GameState.PLAYING:
+		time_remaining -= delta
+		if time_remaining <= 0.0:
+			time_remaining = 0.0
+			_stop_level_timer()
+			if _player:
+				_player.die()
+		else:
+			var current_tick: int = ceili(time_remaining)
+			if current_tick != _last_time_tick:
+				_last_time_tick = current_tick
+				EventBus.time_tick.emit(current_tick)
 
 
 func start_new_game() -> void:
@@ -38,12 +59,15 @@ func respawn_current_level() -> void:
 
 
 func return_to_title() -> void:
+	_stop_level_timer()
+	_player = null
 	set_game_state(GameState.TITLE)
 	get_tree().change_scene_to_file("res://scenes/ui/title_screen.tscn")
 
 
 func reset_for_title() -> void:
 	_reset_run_state()
+	_stop_level_timer()
 	game_state = GameState.TITLE
 
 
@@ -51,20 +75,23 @@ func _enter_level(index: int) -> void:
 	_level_index = index
 	var config := _config.levels[index]
 	set_game_state(GameState.TRANSITIONING)
-	_change_to_level_scene(config)
+	var level_root := _change_to_level_scene(config)
+	_spawn_player(level_root)
 	set_game_state(GameState.PLAYING)
 	_start_level(config)
 
 
 func _start_level(config: Resource) -> void:
+	time_remaining = config.time_limit
+	_timer_active = true
+	_emit_time_tick()
 	if config.music:
 		EventBus.music_requested.emit(config.music)
 	EventBus.level_started.emit(config.display_name)
 
 
-func _change_to_level_scene(config: Resource) -> void:
+func _change_to_level_scene(config: Resource) -> Node:
 	var next_scene: Node = config.scene.instantiate()
-	next_scene.set("level_config", config)
 
 	var tree := get_tree()
 	var old_scene := tree.current_scene
@@ -73,6 +100,29 @@ func _change_to_level_scene(config: Resource) -> void:
 
 	tree.root.add_child(next_scene)
 	tree.current_scene = next_scene
+	return next_scene
+
+
+func _spawn_player(level_root: Node) -> void:
+	var start := level_root.get_node_or_null("PlayerStart") as Marker2D
+	_player = _player_scene.instantiate() as CharacterBody2D
+	if start:
+		_player.position = start.position
+	else:
+		push_warning("No PlayerStart found - spawning player at scene origin")
+	level_root.add_child(_player)
+	_player.died.connect(_on_player_died)
+	_player.death_animation_finished.connect(_on_death_animation_finished)
+
+
+func _stop_level_timer() -> void:
+	_timer_active = false
+
+
+func _emit_time_tick() -> void:
+	var current_tick: int = ceili(time_remaining)
+	_last_time_tick = current_tick
+	EventBus.time_tick.emit(current_tick)
 
 
 func _reset_run_state() -> void:
@@ -128,6 +178,8 @@ func set_game_state(state: GameState) -> void:
 			EventBus.game_paused.emit()
 		GameState.PLAYING:
 			EventBus.game_resumed.emit()
+		GameState.GAME_OVER:
+			_stop_level_timer()
 
 
 func get_current_level() -> Resource:
@@ -142,8 +194,24 @@ func earn_one_up() -> void:
 	EventBus.lives_changed.emit(lives)
 
 
-func _on_level_completed() -> void:
+func _on_player_died() -> void:
+	_stop_level_timer()
 	EventBus.music_stop_requested.emit()
+
+
+func _on_death_animation_finished() -> void:
+	lose_life()
+
+
+func _on_flagpole_reached(_height_ratio: float) -> void:
+	_stop_level_timer()
+
+
+func _on_level_completed() -> void:
+	_stop_level_timer()
+	EventBus.music_stop_requested.emit()
+	var time_bonus := ceili(time_remaining) * 50
+	add_score(time_bonus)
 	set_game_state(GameState.LEVEL_COMPLETE)
 
 
