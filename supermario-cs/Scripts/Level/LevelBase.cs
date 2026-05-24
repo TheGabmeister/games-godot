3a0f78a82106f54542b36c92441b4023393512e0
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-05-24 23:02:55 +0800

    Fix

--- supermario-cs/Scripts/Level/LevelBase.cs
+++ supermario-cs/Scripts/Level/LevelBase.cs
@@ -4,15 +4,6 @@ namespace SuperMario;
 
 public partial class LevelBase : Node2D
 {
-    [Export] private NodePath _playerStartPath;
-    [Export] private NodePath _goalTriggerPath;
-
-    public Marker2D PlayerStart { get; private set; }
-    public GoalTrigger GoalTrigger { get; private set; }
-
-    public override void _Ready()
-    {
-        PlayerStart = GetNode<Marker2D>(_playerStartPath);
-        GoalTrigger = GetNode<GoalTrigger>(_goalTriggerPath);
-    }
+    [Export] public Marker2D PlayerStart;
+    [Export] public GoalTrigger GoalTrigger;
 }
