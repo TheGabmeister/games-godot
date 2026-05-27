7f31f0bad543f0f94c1b9fd3c09bc4e7b397bfa6
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-05-27 08:23:13 +0800

    Optimize

--- supermario-cs/Scripts/Markers/LabeledMarker.cs
+++ supermario-cs/Scripts/Markers/LabeledMarker.cs
@@ -5,6 +5,8 @@ namespace SMB;
 [Tool]
 public partial class LabeledMarker : Marker2D
 {
+    private const int FontSize = 20;
+
     public override void _Draw()
     {
         if (!Engine.IsEditorHint())
@@ -15,10 +17,11 @@ public partial class LabeledMarker : Marker2D
             label = label[..^"Marker".Length];
 
         var font = ThemeDB.FallbackFont;
-        var size = ThemeDB.FallbackFontSize / 4;
-        var textSize = font.GetStringSize(label, HorizontalAlignment.Left, -1, size);
-        var pos = new Vector2(-textSize.X / 2, font.GetAscent(size) / 2);
-        DrawString(font, pos + new Vector2(1, 1), label, HorizontalAlignment.Left, -1, size, Colors.Black);
-        DrawString(font, pos, label, HorizontalAlignment.Left, -1, size, Colors.Yellow);
+        DrawSetTransform(Vector2.Zero, 0, Vector2.One / GetViewportTransform().Scale);
+
+        var textSize = font.GetStringSize(label, HorizontalAlignment.Left, -1, FontSize);
+        var pos = new Vector2(-textSize.X / 2, font.GetAscent(FontSize) / 2);
+        DrawString(font, pos + Vector2.One, label, HorizontalAlignment.Left, -1, FontSize, Colors.Black);
+        DrawString(font, pos, label, HorizontalAlignment.Left, -1, FontSize, Colors.Yellow);
     }
 }
