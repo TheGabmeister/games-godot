b552ca243cd033f72a897ad7727e67ea571838b9
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-05-28 14:57:08 +0800

    Update Config.cs

--- supermario-cs/Scripts/_Core/Config.cs
+++ supermario-cs/Scripts/_Core/Config.cs
@@ -5,8 +5,8 @@ public static class Config
     public const string BootScenePath = "res://Scenes/Boot.tscn";
     public const string MainMenuScenePath = "res://Scenes/MainMenu.tscn";
     public const string GameOverScenePath = "res://Scenes/GameOver.tscn";
-    public const string HudScenePath = "res://scenes/hud.tscn";
-    public const string PlayerScenePath = "res://scenes/player.tscn";
-    public const string FireballScenePath = "res://scenes/entities/fireball.tscn";
-    public const string CampaignPath = "res://resources/campaign.tres";
+    public const string HudScenePath = "res://Scenes/HUD.tscn";
+    public const string PlayerScenePath = "res://Scenes/Player.tscn";
+    public const string FireballScenePath = "res://Scenes/Projectiles/Fireball.tscn";
+    public const string CampaignPath = "res://Resources/Campaign.tres";
 }
